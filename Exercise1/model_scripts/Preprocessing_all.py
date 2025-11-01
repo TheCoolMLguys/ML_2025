2783ab6061f96fc83e0d8417735dda24c5c56b30
KostasDm <kostdam92@gmail.com>
2025-11-01 16:36:02 +0100

    Add script with preprocessing classes and logistic regression script

---
+++ Exercise1/model_scripts/Preprocessing_all.py
@@ -0,0 +1,153 @@
+import pandas as pd 
+import os
+from sklearn.preprocessing import StandardScaler
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+
+
+class Personality_type_preprocessing(BaseEstimator, TransformerMixin):
+
+ # BaseEstimator and TransformerMixin for creating a custom transformer class
+
+  def __init__(self):
+
+    self.scaler = StandardScaler() 
+
+   
+  def impute_data(self, X, Y=None):
+
+     categorical_columns = ['Gender', 'Interest']
+     X_encoded = pd.get_dummies(X, columns=categorical_columns, dtype='int')
+    
+     return X_encoded, Y
+
+
+  def apply_standarization(self, X):
+
+    scaler = StandardScaler()
+  
+    cols_to_scale = X.select_dtypes(include='number').columns
+
+    df_scaled = X.copy()
+
+    df_scaled[cols_to_scale] = scaler.fit_transform(df_scaled[cols_to_scale])
+
+    return df_scaled
+
+  
+  
+  def fit(self, X, Y=None):
+        # Fit the scaler only on training X
+        X_transformed, Y = self.impute_data(X, Y)
+
+        numeric_colums = X_transformed.select_dtypes(include='number').columns
+        self.scaler.fit(X_transformed[numeric_colums])
+        return self
+
+
+  def transform(self, X, Y=None):
+
+        X_transformed, Y = self.impute_data(X, Y)
+        numeric_colums = X_transformed.select_dtypes(include='number').columns
+
+        X_transformed[numeric_colums] = self.scaler.transform(X_transformed[numeric_colums])
+
+        if Y is not None:
+            return X_transformed, Y
+
+        return X_transformed
+
+
+
+
+class Ozone_preprocessing(BaseEstimator, TransformerMixin):
+
+  # BaseEstimator and TransformerMixin for creating a custom transformer class
+
+  def __init__(self):
+
+    self.scaler = StandardScaler()
+
+   
+  def drop_rows_with_majority_NAN(self, X, Y= None):
+
+  
+    drop_index = X.dropna(thresh=X.shape[1]/2).index 
+
+    X = X.loc[drop_index]
+    if Y is not None:
+        Y = Y.loc[drop_index]
+
+    return X, Y
+
+
+
+  def impute_data(self, X, Y= None):
+ 
+    X['Date'] = pd.to_datetime(X['Date'])
+
+    for col in X.columns[1:]:
+       X[col] = pd.to_numeric(X[col], errors="coerce")
+
+    # drop rows with more than 50% of features having missing values
+    #X, Y = self.drop_rows_with_majority_NAN(X, Y)
+
+    X['year_month'] = X['Date'].dt.to_period('M')
+
+    cols_to_impute = X.select_dtypes(include='number').columns
+
+    for col in cols_to_impute:
+       X[col] = X.groupby('year_month')[col].transform(lambda x: x.fillna(x.mean()))
+  
+    # Because some months have no data, instead of dropping them, impute them with the mean of the month over the years
+    X['month'] = X['Date'].dt.month
+    for col in cols_to_impute:
+       X[col] = X.groupby('month')[col].transform(lambda x: x.fillna(x.mean()))
+
+    X = X.drop(columns=['year_month', 'month'])
+    X = X.set_index("Date")
+
+    return X, Y
+
+
+  def apply_standarization(self, X):
+
+    scaler = StandardScaler()
+  
+    #remove Target output from list of columns to be scaled
+    cols_to_scale = X.select_dtypes(include='number').columns
+
+    df_scaled = X.copy()
+
+    df_scaled[cols_to_scale] = scaler.fit_transform(df_scaled[cols_to_scale])
+
+    return df_scaled
+
+  
+  
+  def fit(self, X, Y=None):
+
+        # Fit the scaler only on training X
+        X_transformed, Y = self.impute_data(X, Y)
+    
+        numeric_columns = X_transformed.select_dtypes(include='number').columns
+
+        self.scaler.fit(X_transformed[numeric_columns])
+
+        return self
+
+
+  def transform(self, X, Y=None):
+
+        X_transformed, Y = self.impute_data(X, Y)
+        numeric_columns = X_transformed.select_dtypes(include='number').columns
+        X_transformed[numeric_columns] = self.scaler.transform(X_transformed[numeric_columns])
+
+        if Y is not None:
+            return X_transformed, Y
+
+        return X_transformed
+      
+   
+
---
+++ Exercise1/model_scripts/log_regression.py
@@ -0,0 +1,208 @@
+import pandas as pd 
+import numpy as np 
+from Preprocessing_all import Ozone_preprocessing, Personality_type_preprocessing
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_validate, KFold
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer
+from sklearn.linear_model import LogisticRegression
+import time
+import pickle 
+import os
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+def data_exploration(df, target):
+
+    df_copy = df.copy()
+    if 'Date' in df_copy.columns:
+        df_copy['Date'] = pd.to_datetime(df['Date'])
+        df_copy = df_copy.set_index('Date')
+
+    for col in df_copy.columns:
+        df_copy[col] = pd.to_numeric(df_copy[col], errors="coerce")
+    
+    corr_features = df_copy.drop(columns= target).corr().abs()
+    corr_with_target = df_copy.corr().abs()[target]
+
+    high_corr_with_target = corr_with_target[(corr_with_target.abs() > 0.8)].index.tolist() 
+    low_corr_with_target = corr_with_target[(corr_with_target.abs() < 0.2)].index.tolist()
+    print('High correlation with Target variable: {}'.format(high_corr_with_target))
+    print('Low correlation with Target variable: {}'.format(low_corr_with_target))
+
+
+    upper = corr_features.where(np.triu(np.ones(corr_features.shape), k=1).astype(bool))
+    print(upper)
+
+
+
+
+def split_dataset(df, target_column, test_set_size):
+
+    # Takes as input a dataframe, the target variable and the split size
+    # Returns the train and validation dataset 
+
+    X = df.drop(columns=[target_column])
+    y = df[target_column]
+
+
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=test_set_size, random_state=42)
+
+    return X_train, y_train, X_valid, y_valid
+
+
+def train_dataset(model, X_train, y_train, X_valid):
+
+    #Trains and evaluates a model on validation data.
+
+    start = time.time()
+    model.fit(X_train, y_train)
+    train_time = time.time() - start
+    
+    y_pred = model.predict(X_valid)
+        
+    return y_pred, train_time
+
+
+def evaluate_dataset(y_valid, y_pred, train_time):
+
+
+    return {
+            "accuracy": accuracy_score(y_valid, y_pred),
+            "precision": precision_score(y_valid, y_pred, average='weighted', zero_division=1),
+            "recall": recall_score(y_valid, y_pred, average='weighted', zero_division=1),
+            "f1_score": f1_score(y_valid, y_pred, average='weighted'),
+            "training_time": train_time }
+
+
+def cross_val_metrics():
+      
+        return {
+            "accuracy": 'accuracy',
+            "precision": make_scorer(precision_score, average='weighted', zero_division=1),
+            "recall": make_scorer(recall_score, average='weighted', zero_division=1),
+            "f1": make_scorer(f1_score, average='weighted')}
+
+
+def run(datasets):
+
+    for keys, data in datasets.items():
+        
+        print("#"*30)
+        print('Data Exploration')
+
+        data_exploration(data[0], data[1])
+
+        print("#"*30)
+        print(f"Starting modeling of dataset {keys}")
+
+        preprocessing_class_instance = data[2]()
+
+        # Split data into 80-20%
+        X_train, y_train, X_valid, y_valid = split_dataset(data[0], data[1], 0.2)
+
+        #Set up pipeline with preprocessing class and model
+        pipeline = make_pipeline(preprocessing_class_instance, LogisticRegression(random_state=42))
+ 
+        # if we want to see the output of preprocessing
+        #X_train_scaled, y_train = pipeline.named_steps['ozone_preprocessing'].transform(X_train, y_train)
+        
+        # Train model by using pipeline object with holdout method
+        y_pred, train_time = train_dataset(pipeline, X_train, y_train, X_valid)
+        
+        #Evaluate performance
+        metrics = evaluate_dataset(y_valid, y_pred, train_time)
+
+        print(metrics)
+        
+        #### Cross-validation #####
+
+        #Initiate k-fold cross validation
+        kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+        scorer = cross_val_metrics() 
+
+        # Run cross-validation
+        cv_results = cross_validate(
+            pipeline,
+            X_train,
+            y_train,
+            cv=kf,
+            scoring=scorer,
+            return_train_score=True,
+            n_jobs=-1)
+        
+        for metric in scorer.keys():
+           print(f"Train {metric}: {np.mean(cv_results[f'train_{metric}']):.4f} ± {np.std(cv_results[f'train_{metric}']):.4f}")
+           print(f"Validation {metric}: {np.mean(cv_results[f'test_{metric}']):.4f} ± {np.std(cv_results[f'test_{metric}']):.4f}")
+
+       
+        ### Perform hyper-parameter tuning ####
+       
+        params_grid = [{
+         'logisticregression__solver': ['lbfgs','liblinear'],
+         'logisticregression__tol': [1e-4, 1e-3,  1e-1],
+         'logisticregression__C': [0.001, 0.1, 1, 100, 1000],
+         'logisticregression__max_iter': [100,1000,5000] }]
+
+        grid_search = GridSearchCV(pipeline, params_grid, cv= 5, scoring='accuracy', error_score='raise', n_jobs=-1)
+        grid_search.fit(X_train, y_train)
+     
+
+        #Show all results from Grid Search
+        print( pd.DataFrame(grid_search.cv_results_).columns)
+
+        print('Best parameters for model based on grid search are:')
+        print(grid_search.best_params_)
+        print('Best score for model based on grid search is:')
+        print(grid_search.best_score_)
+
+        # Use best parameters to your model - hold out method
+        print('#'*30)
+        print('Feed best parameters in model')
+
+        model_best_params = make_pipeline(preprocessing_class_instance, LogisticRegression(**{k.split("__")[1]: v for k, v in grid_search.best_params_.items()}))
+        y_pred_best, train_time_best = train_dataset(model_best_params, X_train, y_train, X_valid)
+        
+        #Evaluate performance
+        metrics_best = evaluate_dataset(y_valid, y_pred_best, train_time_best)
+        print('Metrics with hold-out method after using best parameters')
+        print(metrics_best)
+
+
+        # Ozone - remove hourly data
+
+        if keys=='Ozone_level':
+            
+            cols_to_drop = []
+            for i in range(24):
+           
+               cols_to_drop.append(f'WSR{i}')
+               cols_to_drop.append(f'T{i}')
+            df = data[0].drop(columns = cols_to_drop)
+            X_train_red, y_train_red, X_valid_red, y_valid_red = split_dataset(df, data[1], 0.2)
+
+            pipeline_red = make_pipeline(preprocessing_class_instance, LogisticRegression(random_state=42))
+ 
+            y_pred_red, train_time_red = train_dataset(pipeline_red, X_train_red, y_train_red, X_valid_red)
+            metrics_red = evaluate_dataset(y_valid_red, y_pred_red, train_time_red)
+            print('Metrics for dataset with feature reduction:')
+            print(metrics_red)
+
+
+if __name__ == '__main__':
+
+
+    df_ozone = pd.read_csv("data"+ os.sep + "ozone_level_data.csv")
+    df_personality = pd.read_csv("data"+ os.sep + "personality_types_data_v2.csv")
+    
+
+    # Define the dataset dictionary
+
+    datasets = {"Personality_type": [df_personality, "Personality", Personality_type_preprocessing],
+                "Ozone_level": [df_ozone, "Ozone", Ozone_preprocessing]}
+
+    run(datasets)
+    
+        
