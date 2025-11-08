0629291aa480cbfc255f0e016dc97a8d1860a40e
lenibot <helene.vaught@aon.at>
2025-11-08 18:31:46 +0100

    Updated random forest to include balancing and feature selection

--- Exercise1/model_scripts/random_forest.py
+++ Exercise1/model_scripts/random_forest.py
@@ -2,11 +2,13 @@ import pandas as pd
 import numpy as np 
 from Preprocessing_all import Ozone_preprocessing, Personality_type_preprocessing, breast_cancer_preprocessing, loan_preprocessing
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import train_test_split, GridSearchCV, cross_validate, KFold
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_validate, KFold, StratifiedKFold
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer
 from sklearn.ensemble import RandomForestClassifier
+from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import Pipeline
+from sklearn.feature_selection import SelectKBest, f_classif
 import time
-import pickle 
 import os
 import warnings
 
@@ -47,7 +49,7 @@ def split_dataset(df, target_column, test_set_size):
     y = df[target_column]
 
 
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=test_set_size, random_state=42)
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=test_set_size, random_state=42, shuffle=True)
 
     return X_train, y_train, X_valid, y_valid
 
@@ -70,19 +72,27 @@ def evaluate_dataset(y_valid, y_pred, train_time):
 
     return {
             "accuracy": accuracy_score(y_valid, y_pred),
-            "precision": precision_score(y_valid, y_pred, average='weighted', zero_division=1),
-            "recall": recall_score(y_valid, y_pred, average='weighted', zero_division=1),
-            "f1_score": f1_score(y_valid, y_pred, average='weighted'),
-            "training_time": train_time }
+            "precision_macro": precision_score(y_valid, y_pred, average='macro', zero_division=1),
+            "recall_macro": recall_score(y_valid, y_pred, average='macro', zero_division=1),
+            "f1_macro": f1_score(y_valid, y_pred, average='macro'),
+            "precision_weighted": precision_score(y_valid, y_pred, average='weighted', zero_division=1),
+            "recall_weighted": recall_score(y_valid, y_pred, average='weighted', zero_division=1),
+            "f1_weighted": f1_score(y_valid, y_pred, average='weighted'),
+            "training_time": train_time 
+            }
 
 
 def cross_val_metrics():
       
         return {
             "accuracy": 'accuracy',
-            "precision": make_scorer(precision_score, average='weighted', zero_division=1),
-            "recall": make_scorer(recall_score, average='weighted', zero_division=1),
-            "f1": make_scorer(f1_score, average='weighted')}
+            "precision_macro": make_scorer(precision_score, average='macro', zero_division=1),
+            "recall_macro": make_scorer(recall_score, average='macro', zero_division=1),
+            "f1_macro": make_scorer(f1_score, average='macro'),
+            "precision_weighted": make_scorer(precision_score, average='weighted', zero_division=1),
+            "recall_weighted": make_scorer(recall_score, average='weighted', zero_division=1),
+            "f1_weighted": make_scorer(f1_score, average='weighted')
+            }
 
 
 def run(datasets):
@@ -90,9 +100,9 @@ def run(datasets):
     for keys, data in datasets.items():
         
         print("#"*30)
-        print('Data Exploration')
+        #print('Data Exploration')
 
-        data_exploration(data[0], data[1])
+        #data_exploration(data[0], data[1])
 
         print("#"*30)
         print(f"Starting modeling of dataset {keys}")
@@ -104,9 +114,6 @@ def run(datasets):
 
         #Set up pipeline with preprocessing class and model
         pipeline = make_pipeline(preprocessing_class_instance, RandomForestClassifier(random_state=42))
- 
-        # if we want to see the output of preprocessing
-        #X_train_scaled, y_train = pipeline.named_steps['ozone_preprocessing'].transform(X_train, y_train)
         
         # Train model by using pipeline object with holdout method
         y_pred, train_time = train_dataset(pipeline, X_train, y_train, X_valid)
@@ -119,6 +126,8 @@ def run(datasets):
         #### Cross-validation #####
 
         #Initiate k-fold cross validation
+        print('#'*30)
+        print('Results for 5-fold Cross-validation')
         kf = KFold(n_splits=5, shuffle=True, random_state=42)
 
         scorer = cross_val_metrics() 
@@ -136,28 +145,45 @@ def run(datasets):
         for metric in scorer.keys():
            print(f"Train {metric}: {np.mean(cv_results[f'train_{metric}']):.4f} ± {np.std(cv_results[f'train_{metric}']):.4f}")
            print(f"Validation {metric}: {np.mean(cv_results[f'test_{metric}']):.4f} ± {np.std(cv_results[f'test_{metric}']):.4f}")
+           
+           print(f"Mean fit time: {np.mean(cv_results['fit_time']):.3f} s ± {np.std(cv_results['fit_time']):.3f}")
+        
 
-       
+
         ### Perform hyper-parameter tuning ####
+        print('#'*30)
+        print('Results from Grid Search')
        
         params_grid = [{
-         'randomforestclassifier__n_estimators': [150, 300],
+         'randomforestclassifier__n_estimators': [50, 200, 300],
          'randomforestclassifier__max_depth': [None, 15, 30],
-         'randomforestclassifier__min_samples_split': [5, 10],
-         'randomforestclassifier__min_samples_leaf': [1, 2],
-         'randomforestclassifier__max_features': ['sqrt', 'log2'] }]
-
-        grid_search = GridSearchCV(pipeline, params_grid, cv= 5, scoring='accuracy', error_score='raise', n_jobs=-1)
+         'randomforestclassifier__min_samples_split': [2, 10, 0.01],
+         'randomforestclassifier__min_samples_leaf': [1, 5, 30],
+        }]
+        scoring = {'acc': 'accuracy','f1_macro': 'f1_macro', 'precision_macro': 'precision_macro', 'recall_macro': 'recall_macro', 
+                   'f1_w': 'f1_weighted', 'precision_w': 'precision_weighted', 'recall_w': 'recall_weighted'}
+        
+        grid_search = GridSearchCV(pipeline, params_grid, cv = 5, scoring=scoring, refit='f1_macro', error_score='raise', n_jobs=-1)
         grid_search.fit(X_train, y_train)
      
 
         #Show all results from Grid Search
-        print( pd.DataFrame(grid_search.cv_results_).columns)
 
         print('Best parameters for model based on grid search are:')
         print(grid_search.best_params_)
         print('Best score for model based on grid search is:')
         print(grid_search.best_score_)
+
+        results_df = pd.DataFrame(grid_search.cv_results_)
+
+        metric_cols = [col for col in results_df.columns if 'mean_' in col or 'std_' in col]
+        summary_df = results_df[['params'] + metric_cols]
+
+        summary_df = summary_df.sort_values('mean_test_f1_macro', ascending=False)
+
+
+        summary_df.to_csv(f'overview_{keys}.csv', index = False)
+
 
         # Use best parameters to your model - hold out method
         print('#'*30)
@@ -170,6 +196,80 @@ def run(datasets):
         metrics_best = evaluate_dataset(y_valid, y_pred_best, train_time_best)
         print('Metrics with hold-out method after using best parameters')
         print(metrics_best)
+
+
+        #####################################################################
+        ###### Test impact of balancing
+
+        print('#'*30)
+        print('Test impact of balancing')
+
+        pip_balanced = Pipeline([('preprocessing',preprocessing_class_instance), ('smote', SMOTE(random_state=42)), ('model', RandomForestClassifier(random_state=42))])
+        pip_balanced_best = Pipeline([('preprocessing',preprocessing_class_instance), ('smote', SMOTE(random_state=42)), ('model', RandomForestClassifier(**{k.split("__")[1]: v for k, v in grid_search.best_params_.items()}, random_state=42))])
+        kf_balanced = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+
+        cv_results_balanced = cross_validate(
+            pip_balanced,
+            X_train,
+            y_train,
+            cv=kf_balanced,
+            scoring=scorer,
+            return_train_score=True,
+            n_jobs=-1)
+
+        cv_results_balanced_best = cross_validate(
+            pip_balanced_best,
+            X_train,
+            y_train,
+            cv=kf_balanced,
+            scoring=scorer,
+            return_train_score=True,
+            n_jobs=-1)
+        
+        for metric_balanced in scorer.keys():
+           print('Default model parameters after balancing')
+           print(f"Train {metric_balanced}: {np.mean(cv_results_balanced[f'train_{metric_balanced}']):.4f} ± {np.std(cv_results_balanced[f'train_{metric_balanced}']):.4f}")
+           print(f"Validation {metric_balanced}: {np.mean(cv_results_balanced[f'test_{metric_balanced}']):.4f} ± {np.std(cv_results_balanced[f'test_{metric_balanced}']):.4f}")
+           print('Best model parameters after balancing')
+           
+           print(f"Train {metric_balanced}: {np.mean(cv_results_balanced_best[f'train_{metric_balanced}']):.4f} ± {np.std(cv_results_balanced_best[f'train_{metric_balanced}']):.4f}")
+           print(f"Validation {metric_balanced}: {np.mean(cv_results_balanced_best[f'test_{metric_balanced}']):.4f} ± {np.std(cv_results_balanced_best[f'test_{metric_balanced}']):.4f}")
+           print('#'*20)
+
+        print(f"Mean fit time: {np.mean(cv_results_balanced['fit_time']):.3f} s ± {np.std(cv_results_balanced['fit_time']):.3f}")
+        print(f"Mean fit time: {np.mean(cv_results_balanced_best['fit_time']):.3f} s ± {np.std(cv_results_balanced_best['fit_time']):.3f}")
+        
+
+        ###########################################################################
+        ########Test impact of feature reduction 
+
+
+        print('#'*30)
+        print('Test impact of feature reduction')
+
+        pip_reduced =  make_pipeline(preprocessing_class_instance, SelectKBest(score_func=f_classif, k=30) , RandomForestClassifier(random_state=42)) 
+        
+        kf_reduced = KFold(n_splits=5, shuffle=True, random_state=42)
+
+
+        cv_results_reduced = cross_validate(
+            pip_reduced,
+            X_train,
+            y_train,
+            cv=kf_reduced,
+            scoring=scorer,
+            return_train_score=True,
+            n_jobs=-1)
+
+        
+        for metric_reduced in scorer.keys():
+           print('Default model parameters after balancing')
+           print(f"Train {metric_reduced}: {np.mean(cv_results_reduced[f'train_{metric_reduced}']):.4f} ± {np.std(cv_results_reduced[f'train_{metric_reduced}']):.4f}")
+           print(f"Validation {metric_reduced}: {np.mean(cv_results_reduced[f'test_{metric_reduced}']):.4f} ± {np.std(cv_results_reduced[f'test_{metric_reduced}']):.4f}")
+
+
+        print(f"Mean fit time: {np.mean(cv_results_reduced['fit_time']):.3f} s ± {np.std(cv_results_reduced['fit_time']):.3f}")
 
 
         # Ozone - remove hourly data
@@ -188,20 +288,10 @@ def run(datasets):
  
             y_pred_red, train_time_red = train_dataset(pipeline_red, X_train_red, y_train_red, X_valid_red)
             metrics_red = evaluate_dataset(y_valid_red, y_pred_red, train_time_red)
+            print('Special feature reduction analysis for Ozone dataset')
             print('Metrics for dataset with feature reduction:')
             print(metrics_red)
 
-        # figure out how to add that comparison still? AKA switch from non log transform to log transform
-        #if keys=='Breast_cancer':
-            
-            #print("\n Comparison to process with log-transformed variables:")
-            #X_train_blue, y_train_blue, X_valid_blue, y_valid_blue = split_dataset(df, data[1], 0.2)
-
-            #pipeline_blue = make_pipeline(preprocessing_class_instance, RandomForestClassifier(random_state=42))
-            #y_pred_blue, train_time_blue = train_dataset(pipeline_blue, X_train_blue, y_train_blue, X_valid_blue)
-            #metrics_blue = evaluate_dataset(y_valid_blue, y_pred_blue, train_time_blue)
-            #print('Metrics for dataset with feature reduction:')
-            #print(metrics_blue)
 
 
 
@@ -218,8 +308,8 @@ if __name__ == '__main__':
 
     # Define the dataset dictionary
 
-    datasets = {"Personality_type": [df_personality, "Personality", Personality_type_preprocessing],
-                "Ozone_level": [df_ozone, "Ozone", Ozone_preprocessing],
+    datasets = {#"Personality_type": [df_personality, "Personality", Personality_type_preprocessing],
+                #"Ozone_level": [df_ozone, "Ozone", Ozone_preprocessing],
                 "Breast_cancer": [df_breast_cancer, "class", breast_cancer_preprocessing],
                 "Loan": [df_loan, "grade", loan_preprocessing]}
 
