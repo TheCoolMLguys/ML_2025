50c0275f9bcd7e22b6e699c56d94d0e2e9f2c0f2
KostasDm <kostdam92@gmail.com>
2025-11-10 19:33:57 +0100

    Add kaggle competition part

--- Exercise1/model_scripts/log_regression.py
+++ Exercise1/model_scripts/log_regression.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV, cross_valida
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer
 from sklearn.linear_model import LogisticRegression
 from imblearn.over_sampling import SMOTE
-from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.feature_selection import SelectKBest, f_classif, mutual_info_classif
 from imblearn.pipeline import Pipeline
 import time
 import os
@@ -26,10 +26,10 @@ def data_exploration(df, target):
         df_copy[col] = pd.to_numeric(df_copy[col], errors="coerce")
     
     corr_features = df_copy.drop(columns= target).corr().abs()
-    corr_with_target = df_copy.corr().abs()[target]
-
-    high_corr_with_target = corr_with_target[(corr_with_target.abs() > 0.8)].index.tolist() 
-    low_corr_with_target = corr_with_target[(corr_with_target.abs() < 0.2)].index.tolist()
+    corr_with_target = df_copy.corr().abs()['loan_status']
+    
+    high_corr_with_target = corr_with_target[(corr_with_target > 0.8)].index.tolist() 
+    low_corr_with_target = corr_with_target[(corr_with_target < 0.2)].index.tolist()
     print('High correlation with Target variable: {}'.format(high_corr_with_target))
     print('Low correlation with Target variable: {}'.format(low_corr_with_target))
 
@@ -88,18 +88,40 @@ def cross_val_metrics(averaging='macro'):
             "f1": make_scorer(f1_score, average=averaging) }
 
 
+def kaggle_competition(dataset, X_train, y_train, grid_search, name):
+
+    pip_kaggle = make_pipeline(preprocessing_class_instance, LogisticRegression(**{k.split("__")[1]: v for k, v in grid_search.best_params_.items()}, random_state=42))
+   
+    pip_kaggle.fit(X_train, y_train)
+    X_ID = dataset['ID']
+    
+    X= dataset.drop(columns=['ID'])
+
+    
+    y_pred = pip_kaggle.predict(X)        
+
+    pred_df = pd.DataFrame({
+    'ID': X_ID,
+    'class': y_pred
+})
+
+    pred_df.to_csv(f'{name}_prediction.csv', index=False)
+
+
+
 def run(datasets):
 
     for keys, data in datasets.items():
         
         print("#"*30)
-        print('Data Exploration')
+        #print('Data Exploration')
 
-        data_exploration(data[0], data[1])
+       # data_exploration(data[0], data[1])
 
         print("#"*30)
         print(f"Starting modeling of dataset {keys}")
 
+        global preprocessing_class_instance
         preprocessing_class_instance = data[2]()
 
         # Split data into 80-20%
@@ -112,7 +134,7 @@ def run(datasets):
         
         # Train model by using pipeline object with holdout method
         y_pred, train_time = train_dataset(pipeline, X_train, y_train, X_valid)
-        
+        print(X_valid)
         #Evaluate performance
         metrics_macro = evaluate_dataset(y_valid, y_pred, train_time, 'macro')
         metrics_weighted = evaluate_dataset(y_valid, y_pred, train_time, 'weighted')
@@ -178,9 +200,6 @@ def run(datasets):
 
 
 
-
-
-
         ### Perform hyper-parameter tuning ####
         print('#'*30)
         print('Results from Grid Search')
@@ -191,8 +210,9 @@ def run(datasets):
          'logisticregression__max_iter': [100,1000,5000] }]
 
         # To-Do check how many metrics we can give as input in GridSearchCV 
-        scoring = {'acc': 'accuracy','f1_macro': 'f1_macro', 'precision': 'precision_macro', 'recall': 'recall_macro'
-}
+        scoring = {'acc': 'accuracy','f1_macro': 'f1_macro', 'precision': 'precision_macro', 'recall': 'recall_macro'}
+        scoring_weighted = {'acc': 'accuracy','f1_weighted': 'f1_weighted', 'precision': 'precision_weighted', 'recall': 'recall_weighted'}
+    
         grid_search = GridSearchCV(pipeline, params_grid, cv= 5, scoring=scoring, refit='f1_macro',  error_score='raise', n_jobs=-1)
         grid_search.fit(X_train, y_train)
      
@@ -216,7 +236,7 @@ def run(datasets):
 
         summary_df.to_csv(f'overview_{keys}.csv', index = False)
 
-
+        
 
         # Use best parameters to your model - hold out method
         print('#'*30)
@@ -226,9 +246,10 @@ def run(datasets):
         y_pred_best, train_time_best = train_dataset(model_best_params, X_train, y_train, X_valid)
         
         #Evaluate performance
-        metrics_best = evaluate_dataset(y_valid, y_pred_best, train_time_best)
+        metrics_best = evaluate_dataset(y_valid, y_pred_best, train_time_best, 'weighted')
         print('Metrics with hold-out method after using best parameters')
         print(metrics_best)
+     
 
         #####################################################################
         ###### Test impact of balancing
@@ -237,7 +258,7 @@ def run(datasets):
         print('Test impact of balancing')
 
         pip_balanced = Pipeline([('preprocessing',preprocessing_class_instance), ('smote', SMOTE(random_state=42)), ('model', LogisticRegression(random_state=42))])
-        pip_balanced_best = Pipeline([('preprocessing',preprocessing_class_instance), ('smote', SMOTE(random_state=42)), ('model', LogisticRegression(**{k.split("__")[1]: v for k, v in grid_search.best_params_.items()}, random_state=42))])
+
         kf_balanced = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
 
@@ -250,27 +271,15 @@ def run(datasets):
             return_train_score=True,
             n_jobs=-1)
 
-        cv_results_balanced_best = cross_validate(
-            pip_balanced_best,
-            X_train,
-            y_train,
-            cv=kf_balanced,
-            scoring=scorer_macro,
-            return_train_score=True,
-            n_jobs=-1)
+     
         
         for metric_balanced in scorer_macro.keys():
            print('Default model parameters after balancing')
            print(f"Train {metric_balanced}: {np.mean(cv_results_balanced[f'train_{metric_balanced}']):.4f} ± {np.std(cv_results_balanced[f'train_{metric_balanced}']):.4f}")
            print(f"Validation {metric_balanced}: {np.mean(cv_results_balanced[f'test_{metric_balanced}']):.4f} ± {np.std(cv_results_balanced[f'test_{metric_balanced}']):.4f}")
-           print('Best model parameters after balancing')
-           
-           print(f"Train {metric_balanced}: {np.mean(cv_results_balanced_best[f'train_{metric_balanced}']):.4f} ± {np.std(cv_results_balanced_best[f'train_{metric_balanced}']):.4f}")
-           print(f"Validation {metric_balanced}: {np.mean(cv_results_balanced_best[f'test_{metric_balanced}']):.4f} ± {np.std(cv_results_balanced_best[f'test_{metric_balanced}']):.4f}")
-           print('#'*20)
+      
 
         print(f"Mean fit time: {np.mean(cv_results_balanced['fit_time']):.3f} s ± {np.std(cv_results_balanced['fit_time']):.3f}")
-        print(f"Mean fit time: {np.mean(cv_results_balanced_best['fit_time']):.3f} s ± {np.std(cv_results_balanced_best['fit_time']):.3f}")
         
 
         ###########################################################################
@@ -280,7 +289,7 @@ def run(datasets):
         print('#'*30)
         print('Test impact of feature reduction')
 
-        pip_reduced =  make_pipeline(preprocessing_class_instance, SelectKBest(score_func=f_classif, k=30) , LogisticRegression(random_state=42)) 
+        pip_reduced =  make_pipeline(preprocessing_class_instance, SelectKBest(score_func=mutual_info_classif, k=6) , LogisticRegression(random_state=42)) 
         
         kf_reduced = KFold(n_splits=5, shuffle=True, random_state=42)
 
@@ -302,6 +311,26 @@ def run(datasets):
 
 
         print(f"Mean fit time: {np.mean(cv_results_reduced['fit_time']):.3f} s ± {np.std(cv_results_reduced['fit_time']):.3f}")
+
+
+     
+        #####################################
+        ###### Kaggle competition
+
+        print('\n')
+        print('#'*30)
+        print('Kaggle competition part')
+        dataset = pd.DataFrame()
+        if keys == 'Breast_cancer':
+               dataset = pd.read_csv("data"+ os.sep + "breast-cancer-diagnostic.shuf.tes.csv")
+        elif keys == 'Loan':
+               dataset = pd.read_csv("data"+ os.sep + "loan-10k.tes.csv")
+        if dataset.shape[0]:
+
+               kaggle_competition(dataset, X_train, y_train, grid_search, keys) 
+        
+
+
 
   
         # Ozone - remove hourly data
@@ -336,9 +365,9 @@ if __name__ == '__main__':
 
     # Define the dataset dictionary
 
-    datasets = {#"Personality_type": [df_personality, "Personality", Personality_type_preprocessing],
-                #"Ozone_level": [df_ozone, "Ozone", Ozone_preprocessing],
-               # "Breast_cancer": [df_breast_cancer, "class", breast_cancer_preprocessing],
+    datasets = {"Personality_type": [df_personality, "Personality", Personality_type_preprocessing],
+                "Ozone_level": [df_ozone, "Ozone", Ozone_preprocessing],
+                "Breast_cancer": [df_breast_cancer, "class", breast_cancer_preprocessing],
                 "Loan": [df_loan, "grade", loan_preprocessing]
                 }
 
