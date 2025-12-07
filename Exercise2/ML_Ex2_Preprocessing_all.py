4848a298110425acb021b4ac1ab1e182d7dd5568
lenibot <helene.vaught@aon.at>
2025-12-07 11:55:15 +0100

    Preprocessing and Comparison scripts + data sets

---
+++ Exercise2/ML_Ex2_Preprocessing_all.py
@@ -0,0 +1,122 @@
+import pandas as pd 
+import os
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder, OneHotEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
+
+class House_Price_preprocessing(BaseEstimator, TransformerMixin):
+    
+    def __init__(self):
+          
+        self.scaler = StandardScaler() 
+        self.oe = None
+        self.onehot = None
+        self.onehot_vars = None
+    
+    # drop ID column, not of any information value
+    # One Hot Encoding
+    def simple_preprocess(self, X):
+        
+        X = X.drop(columns=["Id"], errors="ignore").copy()
+
+        # Ordinal 
+        X["Condition"] = self.oe.transform(X[["Condition"]])
+
+        # One Hot encoding 
+        onehot_encoded = self.onehot.transform(X[self.onehot_vars])
+        onehot_cols = self.onehot.get_feature_names_out(self.onehot_vars)
+        onehot_df = pd.DataFrame(onehot_encoded, columns=onehot_cols, index=X.index)
+
+        X = X.drop(columns=self.onehot_vars)
+        X = pd.concat([X, onehot_df], axis=1)
+
+        return X 
+
+    def fit(self, X, Y=None):
+
+        # Ordinal encoding: Condition
+        ord_categories = [["Poor", "Fair", "Good", "Excellent"]]
+        self.oe = OrdinalEncoder(categories=ord_categories)
+        self.oe.fit(X[["Condition"]])
+
+        # One Hot encoding
+        cat_cols = X.select_dtypes(include="object").columns.tolist()
+        self.onehot_vars = [c for c in cat_cols if c != "Condition"]
+        self.onehot = OneHotEncoder(drop="if_binary", sparse_output=False)
+        self.onehot.fit(X[self.onehot_vars])
+
+        # transform
+        X_transformed = self.simple_preprocess(X)
+
+        # scaling
+        numeric_columns = X_transformed.select_dtypes(include="number").columns
+        self.scaler.fit(X_transformed[numeric_columns])
+
+        return self
+
+
+    def transform(self, X, Y=None):
+
+        X_transformed = self.simple_preprocess(X)
+
+        numeric_columns = X_transformed.select_dtypes(include="number").columns
+        X_transformed[numeric_columns] = self.scaler.transform(X_transformed[numeric_columns])
+
+        if Y is not None:
+            return X_transformed, Y
+
+        return X_transformed
+
+class Phone_Addiction_preprocessing(BaseEstimator, TransformerMixin):
+
+    def __init__(self):
+          
+        self.scaler = StandardScaler() 
+        self.oe = None
+        self.onehot = None
+        self.onehot_vars = None
+    
+    def simple_preprocess(self, X):
+        
+        # drop identifiers
+        X = X.drop(columns=["ID", "Name", "Location"], errors="ignore").copy()
+        # make school grade numeric
+        X['School_Grade'] = X['School_Grade'].str.extract(r"(\d+)").astype(int)
+
+        # One Hot encoding 
+        onehot_encoded = self.onehot.transform(X[self.onehot_vars])
+        onehot_cols = self.onehot.get_feature_names_out(self.onehot_vars)
+        onehot_df = pd.DataFrame(onehot_encoded, columns=onehot_cols, index=X.index)
+
+        X = X.drop(columns=self.onehot_vars)
+        X = pd.concat([X, onehot_df], axis=1)
+
+        return X 
+
+    def fit(self, X, Y=None):
+
+        self.onehot_vars = [c for c in X.select_dtypes(include="object").columns if c not in  ["ID", "Name", "Location", "School_Grade"]]
+        self.onehot = OneHotEncoder(drop="if_binary", sparse_output=False)
+        self.onehot.fit(X[self.onehot_vars])
+
+        # transform
+        X_transformed = self.simple_preprocess(X)
+
+        # scaling
+        numeric_columns = X_transformed.select_dtypes(include="number").columns
+        self.scaler.fit(X_transformed[numeric_columns])
+
+        return self
+
+
+    def transform(self, X, Y=None):
+
+        X_transformed = self.simple_preprocess(X)
+
+        numeric_columns = X_transformed.select_dtypes(include="number").columns
+        X_transformed[numeric_columns] = self.scaler.transform(X_transformed[numeric_columns])
+
+        if Y is not None:
+            return X_transformed, Y
+
+        return X_transformed
---
+++ Exercise2/ML_Ex2_comparisons.py
@@ -0,0 +1,159 @@
+import pandas as pd
+from ML_Ex2_Preprocessing_all import House_Price_preprocessing, Phone_Addiction_preprocessing
+from sklearn.model_selection import train_test_split, cross_validate, KFold
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import make_scorer, mean_squared_error, mean_absolute_error, r2_score
+import time
+import os
+import numpy as np
+
+def split_dataset(df, target_column, test_set_size):
+
+    # Takes as input a dataframe, the target variable and the split size
+    # Returns the train and validation dataset 
+
+    X = df.drop(columns=[target_column])
+    y = df[target_column]
+
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=test_set_size, random_state=42) 
+
+    return X_train, y_train, X_valid, y_valid
+
+def train_dataset(model, X_train, y_train, X_valid):
+
+    #Trains and evaluates a model on validation data.
+
+    start = time.time()
+    model.fit(X_train, y_train)
+    train_time = time.time() - start
+    
+    y_pred = model.predict(X_valid)
+        
+    return y_pred, train_time
+
+def evaluate_dataset(y_valid, y_pred, train_time):
+     
+    return {
+        "MSE": mean_squared_error(y_valid, y_pred),
+        "RMSE": np.sqrt(mean_squared_error(y_valid, y_pred)),
+        "MAE": mean_absolute_error(y_valid, y_pred),
+        "R2": r2_score(y_valid, y_pred),
+        "training_time": train_time
+    }
+
+def cross_val_metrics():
+
+    return {
+        "MSE": make_scorer(lambda y_true, y_pred: mean_squared_error(y_true, y_pred), greater_is_better=False),
+        "RMSE": make_scorer(lambda y_true, y_pred: np.sqrt(mean_squared_error(y_true, y_pred)), greater_is_better=False),
+        "MAE": make_scorer(mean_absolute_error, greater_is_better=False),
+        "R2": "r2" 
+    }
+
+def run(datasets):
+
+    for keys, data in datasets.items():
+
+        global preprocessing_class_instance
+        preprocessing_class_instance = data[2]()
+
+        print("#"*50)
+        print(f"Gradient Boosting Regressor")
+        print("#"*50)
+
+        print("#"*30)
+        print(f"HOLD OUT on {keys}")
+        print("#"*30)
+
+        # Split data into 80-20%
+        X_train, y_train, X_valid, y_valid = split_dataset(data[0], data[1], 0.2)
+
+        #Set up pipeline with preprocessing class and model
+        pipeline = make_pipeline(preprocessing_class_instance, GradientBoostingRegressor(random_state=42)) 
+ 
+        
+        # Train model by using pipeline object with holdout method
+        y_pred, train_time = train_dataset(pipeline, X_train, y_train, X_valid)
+    
+        #Evaluate performance
+        metrics = evaluate_dataset(y_valid, y_pred, train_time)
+    
+        print({k: round(v, 3) for k, v in metrics.items()})
+
+        print("#"*30)
+        print(f"CROSS VALIDATION on {keys}")
+        print("#"*30)
+
+        kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+        scorer = cross_val_metrics() 
+
+        # Run cross-validation
+        cv_results = cross_validate(
+            pipeline,
+            X_train,
+            y_train,
+            cv=kf,
+            scoring=scorer,
+            return_train_score=True,
+            n_jobs=-1)
+        
+        for metric in scorer.keys():
+           print(f"Train {metric}: {np.mean(cv_results[f'train_{metric}']):.4f} ± {np.std(cv_results[f'train_{metric}']):.4f}")
+           print(f"Validation {metric}: {np.mean(cv_results[f'test_{metric}']):.4f} ± {np.std(cv_results[f'test_{metric}']):.4f}")
+        
+        print(f"Mean fit time: {np.mean(cv_results['fit_time']):.3f} s ± {np.std(cv_results['fit_time']):.3f}")
+
+
+        print("#"*50)
+        print(f"Sklearn RF Regression")
+        print("#"*50)
+
+        print("#"*30)
+        print(f"HOLD OUT on {keys}")
+        print("#"*30)
+
+        pipeline = make_pipeline(preprocessing_class_instance, RandomForestRegressor(max_depth=10, random_state=42))
+        
+        # Train model by using pipeline object with holdout method
+        y_pred, train_time = train_dataset(pipeline, X_train, y_train, X_valid)
+        
+        #Evaluate performance
+        metrics = evaluate_dataset(y_valid, y_pred, train_time)
+
+        print({k: round(v, 3) for k, v in metrics.items()})
+        
+        print("#"*30)
+        print(f"CROSS VALIDATION on {keys}")
+        print("#"*30)
+
+        # Run cross-validation
+        cv_results = cross_validate(
+            pipeline,
+            X_train,
+            y_train,
+            cv=kf,
+            scoring=scorer,
+            return_train_score=True,
+            n_jobs=-1)
+        
+        for metric in scorer.keys():
+           print(f"Train {metric}: {np.mean(cv_results[f'train_{metric}']):.4f} ± {np.std(cv_results[f'train_{metric}']):.4f}")
+           print(f"Validation {metric}: {np.mean(cv_results[f'test_{metric}']):.4f} ± {np.std(cv_results[f'test_{metric}']):.4f}")
+        
+        print(f"Mean fit time: {np.mean(cv_results['fit_time']):.3f} s ± {np.std(cv_results['fit_time']):.3f}")
+
+if __name__ == '__main__':
+
+
+    df_houseprice = pd.read_csv("data"+ os.sep + "House_Price_Prediction_Dataset.csv")
+    df_phone_addiction = pd.read_csv("data" + os.sep + "teen_phone_addiction_dataset.csv")
+
+    # Define the dataset dictionary
+
+    datasets = {"House_Price": [df_houseprice, "Price", House_Price_preprocessing],
+                "Phone_Addiction": [df_phone_addiction, "Addiction_Level", Phone_Addiction_preprocessing]}
+
+    run(datasets)
+
