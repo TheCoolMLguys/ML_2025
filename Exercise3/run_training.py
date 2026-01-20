9e31a9f35a92bb1e8b115f6edccba83156e6825a
KostasDm <kostdam92@gmail.com>
2026-01-20 14:40:45 +0100

    Add full version of run_training script

--- Exercise3/run_training.py
+++ Exercise3/run_training.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import numpy as np 
-from preprocessing import breast_cancer_preprocessing, Phone_Addiction_preprocessing, Student_placement_preprocessing
+from preprocessing import breast_cancer_preprocessing, Phone_Addiction_preprocessing, Student_placement_preprocessing, SaNGreeATransformer
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split, cross_validate, KFold
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer
@@ -45,72 +45,103 @@ def train_dataset_holdout(model, X_train, y_train, X_valid):
 
 
 
-def cross_val_metrics():
-      
-        return {
-            "accuracy": 'accuracy',
-            "precision_macro": make_scorer(precision_score, average='macro', zero_division=1),
-            "recall_macro": make_scorer(recall_score, average='macro', zero_division=1),
-            "f1_macro": make_scorer(f1_score, average='macro'),
-            "precision_weighted": make_scorer(precision_score, average='weighted', zero_division=1),
-            "recall_weighted": make_scorer(recall_score, average='weighted', zero_division=1),
-            "f1_weighted": make_scorer(f1_score, average='weighted')
+def evaluate_dataset(y_valid, y_pred, train_time):
+
+
+    return {
+            "accuracy": accuracy_score(y_valid, y_pred),
+            "precision_macro": precision_score(y_valid, y_pred, average='macro', zero_division=1),
+            "recall_macro": recall_score(y_valid, y_pred, average='macro', zero_division=1),
+            "f1_macro": f1_score(y_valid, y_pred, average='macro'),
+            "precision_weighted": precision_score(y_valid, y_pred, average='weighted', zero_division=1),
+            "recall_weighted": recall_score(y_valid, y_pred, average='weighted', zero_division=1),
+            "f1_weighted": f1_score(y_valid, y_pred, average='weighted'),
+            "training_time": train_time 
             }
 
 
-def run(datasets):
+def find_mean_values(metrics_list):
 
+
+    metrics = ["accuracy", "precision_macro", "recall_macro", "f1_macro", "precision_weighted", "recall_weighted", "f1_weighted", "training_time"]
+
+    mean_results = {} 
+
+    collected = {m: [] for m in metrics}
+
+    for entry in metrics_list:
+        for m in metrics:
+            collected[m].append(entry[m])
+
+    for m in metrics:
+        values = np.array(collected[m])
+        mean_results[m] = {"mean": np.mean(values), 
+                           "std": np.std(values, ddof=1)}  # sample standard deviation}
+
+    return mean_results
+
+
+def run(datasets, state, anonymity = True):
+
+    
+    print(f"Anonymization: {anonymity}")
 
     for keys, data in datasets.items():
         
         print("#"*30)
-
-        print("#"*30)
         print(f"Starting modeling of dataset {keys}")
 
-        preprocessing_class_instance = data[2]()
+        preprocessing_class_instance = data[2]()  # instantiate preprocessing class
         
         # Split data into 80-20%
         X_train, y_train, X_valid, y_valid = split_dataset(data[0], data[1], 0.2)
         
+
         models = {"RandomForest": RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42),
                   "LogisticRegression": LogisticRegression(max_iter=1000, random_state=42),
                   "KNN": KNeighborsClassifier(n_neighbors=5)}
 
-        for name, model in models.items():
+        # Iterate over all classifiers
 
+        for name, classifier in models.items():
+       
+          print(f"Training with {name}")
 
-           #Set up pipeline with preprocessing class and model
-           pipeline = make_pipeline(preprocessing_class_instance, model)
-        
-           print(f"Model: {name}")
-           #### Cross-validation #####
+          kf = KFold(n_splits=5, shuffle=True, random_state=state)
 
-           #Initiate k-fold cross validation
-           print('#'*30)
-           print('Results for 5-fold Cross-validation')
-           kf = KFold(n_splits=5, shuffle=True, random_state=42)
+          score_list = []
 
-           scorer = cross_val_metrics() 
+          for train_idx, val_idx in kf.split(X_train):
 
-           # Run cross-validation
-           cv_results = cross_validate(pipeline,
-                                       X_train,
-                                       y_train,
-                                       cv=kf,
-                                       scoring=scorer,
-                                       return_train_score=True,
-                                       n_jobs=-1)
-        
-           for metric in scorer.keys():
-               print(f"Train {metric}: {np.mean(cv_results[f'train_{metric}']):.4f} ± {np.std(cv_results[f'train_{metric}']):.4f}")
-               print(f"Validation {metric}: {np.mean(cv_results[f'test_{metric}']):.4f} ± {np.std(cv_results[f'test_{metric}']):.4f}")
-           
-               print(f"Mean fit time: {np.mean(cv_results['fit_time']):.3f} s ± {np.std(cv_results['fit_time']):.3f}")
+            X_train_cv, X_val_cv = X_train.iloc[train_idx].reset_index(drop=True), X_train.iloc[val_idx].reset_index(drop=True)
+            y_train_cv, y_val_cv = y_train.iloc[train_idx].reset_index(drop=True), y_train.iloc[val_idx].reset_index(drop=True)
 
+            preprocess = preprocessing_class_instance
+            X_train_prep = preprocess.fit_transform(X_train_cv)
+            X_val_prep   = preprocess.transform(X_val_cv)
+       
+            
+            if anonymity:
+               sangreea = SaNGreeATransformer(k=5)
+               sangreea.fit(X_train_prep)
+               X_train_anon = sangreea.transform(X_train_prep)
+         
+            else:
+                X_train_anon = X_train_prep.copy()
 
+            model = classifier
 
+            start = time.time()
+            model.fit(X_train_anon, y_train_cv)
+            train_time = time.time() - start
 
+            y_pred = model.predict(X_val_prep)
+ 
+            scores = evaluate_dataset(y_val_cv, y_pred, train_time)
+            score_list.append(scores)
+
+          print(find_mean_values(score_list))
+     
 
 if __name__ == '__main__':
 
@@ -128,4 +159,6 @@ if __name__ == '__main__':
                 "Teen Addiction": [df_teen_addiction, "Addiction_Level", Phone_Addiction_preprocessing],
                 "Student Placement": [df_student_placement, "Placement_Status", Student_placement_preprocessing]}
 
-    run(datasets)
+    run(datasets, state=42, anonymity = False)
+
+    run(datasets, state=42, anonymity = True)
